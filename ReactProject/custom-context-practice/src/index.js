090630f41bcd352fab7c7db7b0b8d93219aa7e57
RakheeMistry <95229428+RakheeMistry@users.noreply.github.com>
2024-02-26 07:35:57 +0530

    Delete ReactProject directory

--- ReactProject/custom-context-practice/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { CustomProvider } from './components/login-logout-task/credentialContext';
-
-ReactDOM.render(<CustomProvider><App/></CustomProvider>,document.getElementById('root'));
-
- 
